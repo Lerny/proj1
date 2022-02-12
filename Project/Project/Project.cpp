becce96a2eb10945aecbea6f1e2c14c205c90cc9
Lerny <lerny@yandex.ru>
2022-02-13 01:49:24 +0300

    Triangle

--- Project/Project/Project.cpp
+++ Project/Project/Project.cpp
@@ -7,11 +7,11 @@ void display() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1.0, 0.0, 0.0);
 
-	glBegin(GL_POINTS);
-	glVertex2f(10.0, 10.0);
-	glVertex2f(150.0, 80.0);
-	glVertex2f(100.0, 20.0);
-	glVertex2f(200.0, 100.0);
+	glBegin(GL_TRIANGLES);
+	glColor3f(0.1, 0.2, 0.3);
+	glVertex3f(100, 100, 0);
+	glVertex3f(200, 100, 0);
+	glVertex3f(200, 200, 0);
 	glEnd();
 	glFlush();
 }
@@ -34,7 +34,7 @@ int main(int argc, char** argv)
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(0, 0);
-	glutCreateWindow("Points");
+	glutCreateWindow("Triangle");
 	glutDisplayFunc(display);
 
 	myinit();
