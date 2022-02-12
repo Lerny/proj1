31a624887449c73a051024f0d17f66d397488443
Lerny <lerny@yandex.ru>
2022-02-13 02:23:46 +0300

    Rotation

--- Project/Project/Project.cpp
+++ Project/Project/Project.cpp
@@ -3,26 +3,56 @@
 #include "..\glut\include\GL\freeglut.h"
 #include <iostream>
 
+float Angle = 0;
+
 void display() {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1.0, 0.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-1024, 1024, -768, 768, -1000.0f, 1000.0f);
+
+	glRotatef(Angle, 1.0f, 1.0f, 1.0f);
+	Angle += 0.01;
 
 	glBegin(GL_TRIANGLES);
 	glColor3f(0.1, 0.2, 0.3);
-	glVertex3f(100, 100, 0);
-	glVertex3f(200, 100, 0);
-	glVertex3f(200, 200, 0);
+	glVertex3f(100.0f, 100.0f, 0);
+	glVertex3f(200.0f, 100.0f, 0);
+	glVertex3f(200.0f, 200.0f, 0);
 	glEnd();
-	glFlush();
-}
 
-void myinit() {
-	glClearColor(1.0, 1.0, 1.0, 1.0);
-	glColor3f(1.0, 0.0, 0.0);
-	glPointSize(5.0);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(0.0, 499.0, 0.0, 499.0);
+	glBegin(GL_LINES);
+	//Оси
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(1000.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, 1000.0f, 0.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, 0.0f, 1000.0f);
+	//Буква X
+	glVertex3f(1010.0f, 15.0f, 0.0f);
+	glVertex3f(1030.0f, -15.0f, 0.0f);
+	glVertex3f(1010.0f, -15.0f, 0.0f);
+	glVertex3f(1030.0f, 15.0f, 0.0f);
+	//Буква Y
+	glVertex3f(0.0f, 1010.0f, 0.0f);
+	glVertex3f(0.0f, 1030.0f, 0.0f);
+	glVertex3f(0.0f, 1030.0f, 0.0f);
+	glVertex3f(-10.0f, 1050.0f, 0.0f);
+	glVertex3f(0.0f, 1030.0f, 0.0f);
+	glVertex3f(10.0f, 1050.0f, 0.0f);
+	//Буква Z
+	glVertex3f(-15.0f, 0.0f, 1010.0f);
+	glVertex3f(15.0f, 0.0f, 1010.0f);
+	glVertex3f(-15.0f, 0.0f, 1050.0f);
+	glVertex3f(15.0f, 0.0f, 1050.0f);
+	glVertex3f(-15.0f, 0.0f, 1010.0f);
+	glVertex3f(15.0f, 0.0f, 1050.0f);
+	glEnd();
+
+	glFlush();
 }
 
 int main(int argc, char** argv)
@@ -32,23 +62,13 @@ int main(int argc, char** argv)
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(500, 500);
+	glutInitWindowSize(1024, 1024);
 	glutInitWindowPosition(0, 0);
 	glutCreateWindow("Triangle");
+	
 	glutDisplayFunc(display);
+	glutIdleFunc(display);
 
-	myinit();
 	glutMainLoop();
 	return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
