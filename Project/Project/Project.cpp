7fb1b1396dcbb64d12b2b933df61330e63d772ad
Lerny <lerny@yandex.ru>
2022-02-13 01:41:54 +0300

    Opengl Start

---
+++ Project/Project/Project.cpp
@@ -0,0 +1,54 @@
+﻿// Project.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include "..\glut\include\GL\freeglut.h"
+#include <iostream>
+
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 0.0, 0.0);
+
+	glBegin(GL_POINTS);
+	glVertex2f(10.0, 10.0);
+	glVertex2f(150.0, 80.0);
+	glVertex2f(100.0, 20.0);
+	glVertex2f(200.0, 100.0);
+	glEnd();
+	glFlush();
+}
+
+void myinit() {
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glColor3f(1.0, 0.0, 0.0);
+	glPointSize(5.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, 499.0, 0.0, 499.0);
+}
+
+int main(int argc, char** argv)
+{
+	HWND hConsole = GetConsoleWindow();
+	ShowWindow(hConsole, SW_HIDE);//прячем оконо консоли
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Points");
+	glutDisplayFunc(display);
+
+	myinit();
+	glutMainLoop();
+	return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
